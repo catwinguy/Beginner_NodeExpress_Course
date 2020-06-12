053e4526b8b7cdf07420c479dd705edb5f930db2
Cat Nguyen <ctn57d@gmail.com>
2020-06-12 16:52:05 -0400

    Added Get Users

--- server.js
+++ server.js
@@ -10,12 +10,30 @@ const mockUserData = [
     }
 ]
 
+app.get('/',function(req,res)
+{
+    res.json({
+        sucess: true,
+        message: 'Root URL'
+    })
+})
+
 app.get('/users',function(req,res)
 {
     res.json({
         success: true,
         message: 'Successfully got users. Awesome!',
         users: mockUserData
+    })
+})
+
+app.get('/users/:id', function(req,res)
+{
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'Got One User',
+        user: req.params.id
     })
 })
 
