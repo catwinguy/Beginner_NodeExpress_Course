b38bc03ea251f4dbc5b2677cbd45a043dbbff27a
Cat Nguyen <ctn57d@gmail.com>
2020-06-12 16:32:39 -0400

    Added mock user data

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');  // Gives access to express library
 const app = express();  // Constructs an instance of express, named app
 
+const mockUserData = [
+    {
+        name : 'Mark'
+    },
+    {
+        name : 'Jill'
+    }
+]
+
+app.get('/users',function(req,res)
+{
+    res.json({
+        success: true,
+        message: 'Successfully got users. Awesome!',
+        users: mockUserData
+    })
+})
+
 app.listen(8000,function()
 {
     console.log("Server is running...")
